0b3d4ee4af2272d176cb6db18866a17976a07fbb
yyxi <178086254+yyxi@users.noreply.github.com>
2020-06-25 11:03:48 +0400

    fix: do not mutate the model

--- src/state-machine.ts
+++ src/state-machine.ts
@@ -1,6 +1,14 @@
 /* eslint-disable @typescript-eslint/prefer-includes, @typescript-eslint/consistent-type-assertions, @typescript-eslint/no-explicit-any */
 
 import {
+  ACTION_EXISTS,
+  ACTION_UNKNOWN,
+  STATE_EXISTS,
+  STATE_UNKNOWN
+} from './error'
+import { product } from './product'
+import { szudzik } from './szudzik'
+import {
   ActionTransition,
   Fluent,
   Model,
@@ -11,17 +19,11 @@ import {
   SYMBOL_STATE,
   TypeAction
 } from './types'
-import { szudzik } from './szudzik'
-import { product } from './product'
-import {
-  STATE_EXISTS,
-  ACTION_EXISTS,
-  STATE_UNKNOWN,
-  ACTION_UNKNOWN
-} from './error'
 
-const reducerFactory = (model: Model) => (action: StateMachineAction) => {
-  model.log.unshift(action)
+const reduce = (_model: Model, action: StateMachineAction) => {
+  const model = { ..._model }
+
+  model.log = [action, ...model.log]
 
   switch (action.type) {
     case TypeAction.State: {
@@ -29,7 +31,10 @@ const reducerFactory = (model: Model) => (action: StateMachineAction) => {
         return STATE_EXISTS()
       }
 
-      model.state.states.push(action.payload.state)
+      model.state = {
+        ...model.state,
+        states: [...model.state.states, action.payload.state]
+      }
       break
     }
     case TypeAction.Action: {
@@ -37,11 +42,19 @@ const reducerFactory = (model: Model) => (action: StateMachineAction) => {
         return ACTION_EXISTS()
       }
 
-      model.state.actions.push(action.payload.action)
+      model.state = {
+        ...model.state,
+        actions: [...model.state.actions, action.payload.action]
+      }
+
       break
     }
     case TypeAction.Context: {
-      model.state.context = action.payload.context
+      model.state = {
+        ...model.state,
+        context: action.payload.context
+      }
+
       break
     }
     case TypeAction.InitialState: {
@@ -49,7 +62,7 @@ const reducerFactory = (model: Model) => (action: StateMachineAction) => {
         return STATE_UNKNOWN()
       }
 
-      model.state.initial = action.payload
+      model.state = { ...model.state, initial: action.payload }
       break
     }
     case TypeAction.Transition: {
@@ -63,119 +76,129 @@ const reducerFactory = (model: Model) => (action: StateMachineAction) => {
 
       const indexTransition = szudzik(indexSource, indexAction)
       let transitions: Array<ActionTransition['payload']>
+
+      model.state = {
+        ...model.state,
+        transitions: new Map(model.state.transitions)
+      }
+
       const query = model.state.transitions.get(indexTransition)
 
       if (query === undefined) {
         transitions = [action.payload]
-        model.state.transitions.set(indexTransition, transitions)
       } else {
-        transitions = query
+        transitions = [...query]
 
         if (transitions.indexOf(action.payload) === -1) {
           transitions.push(action.payload)
         }
       }
 
+      model.state.transitions.set(indexTransition, transitions)
+
       break
     }
   }
+
+  return model
 }
 
-export const stateMachine = (): Fluent<Next, 'state'> => {
-  const model: Model = {
+const state = (model: Model) => (arg: Placeholder) => {
+  const next = reduce(model, {
+    type: TypeAction.State,
+    payload: {
+      state: arg
+    }
+  })
+
+  return { state: state(next), initial: initial(next) }
+}
+
+const action = (model: Model) => (arg: Placeholder) => {
+  const next = reduce(model, {
+    type: TypeAction.Action,
+    payload: {
+      action: arg
+    }
+  })
+
+  return {
+    transition: transition(next),
+    action: action(next),
+    context: context(next)
+  }
+}
+
+const context = (model: Model) => (arg: unknown) => {
+  const next = reduce(model, {
+    type: TypeAction.Context,
+    payload: {
+      context: arg
+    }
+  })
+
+  return { transition: transition(next) }
+}
+
+const transition = (model: Model) => (
+  source: Placeholder | Placeholder[],
+  action: Placeholder | [Placeholder, ...Array<(...args: any[]) => boolean>],
+  target: Placeholder | Placeholder[],
+  reducer?: (...args: any[]) => unknown
+) => {
+  const ap = Array.isArray(action)
+    ? {
+        action: action[0],
+        predicates: action.slice(1) as Array<(...args: any[]) => boolean>
+      }
+    : { action, predicates: [] }
+
+  if (model.state.actions.indexOf(ap.action) === -1) {
+    return ACTION_UNKNOWN()
+  }
+
+  const next = product(
+    Array.isArray(source) ? source : [source],
+    Array.isArray(target) ? target : [target]
+  ).reduce<Model>((acc, [source, target]) => {
+    return reduce(acc, {
+      type: TypeAction.Transition,
+      payload: {
+        source,
+        target,
+        reducer,
+        ...ap
+      }
+    })
+  }, model)
+
+  return {
+    transition: transition(next),
+    [SYMBOL_LOG]: [...next.log],
+    [SYMBOL_STATE]: { ...next.state }
+  }
+}
+
+const initial = (model: Model) => (arg: Placeholder) => {
+  const next = reduce(model, {
+    type: TypeAction.InitialState,
+    payload: arg
+  })
+
+  return { action: action(next) }
+}
+
+export const stateMachine = (
+  model: Model = {
     log: [],
     state: {
       initial: undefined,
       states: [],
       actions: [],
-      // contexts: {},
       context: undefined,
       transitions: new Map()
     }
   }
-
-  const reduce = reducerFactory(model)
-
-  const state = (arg: Placeholder) => {
-    reduce({
-      type: TypeAction.State,
-      payload: {
-        state: arg
-      }
-    })
-
-    return { state, initial }
-  }
-
-  const action = (arg: Placeholder) => {
-    reduce({
-      type: TypeAction.Action,
-      payload: {
-        action: arg
-      }
-    })
-
-    return { transition, action, context }
-  }
-
-  const context = (arg: unknown) => {
-    reduce({
-      type: TypeAction.Context,
-      payload: {
-        context: arg
-      }
-    })
-
-    return { transition }
-  }
-
-  const transition = (
-    source: Placeholder | Placeholder[],
-    action: Placeholder | [Placeholder, ...Array<(...args: any[]) => boolean>],
-    target: Placeholder | Placeholder[],
-    reducer?: (...args: any[]) => unknown
-  ) => {
-    const ap = Array.isArray(action)
-      ? {
-          action: action[0],
-          predicates: action.slice(1) as Array<(...args: any[]) => boolean>
-        }
-      : { action, predicates: [] }
-
-    if (model.state.actions.indexOf(ap.action) === -1) {
-      return ACTION_UNKNOWN()
-    }
-
-    product(
-      Array.isArray(source) ? source : [source],
-      Array.isArray(target) ? target : [target]
-    ).forEach(([source, target]) =>
-      reduce({
-        type: TypeAction.Transition,
-        payload: {
-          source,
-          target,
-          reducer,
-          ...ap
-        }
-      })
-    )
-
-    return {
-      transition,
-      [SYMBOL_LOG]: model.log,
-      [SYMBOL_STATE]: model.state
-    }
-  }
-
-  const initial = (arg: Placeholder) => {
-    reduce({
-      type: TypeAction.InitialState,
-      payload: arg
-    })
-
-    return { action }
-  }
-
-  return ({ state } as unknown) as Fluent<Next, 'state'>
+): Fluent<Next, 'state'> => {
+  return ({ state: state(model) } as unknown) as Fluent<Next, 'state'>
 }
