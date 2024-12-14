e9e84813df915d0ea2b549b957345659fa71b033
yyxi <178086254+yyxi@users.noreply.github.com>
2024-12-14 09:06:10 +0000

    fix: upkeep

--- src/szudzik.ts
+++ src/szudzik.ts
@@ -1,2 +1,1 @@
-export const szudzik = (a: number, b: number): number =>
-  a >= b ? a * a + a + b : a + b * b
+export const szudzik = (a: number, b: number): number => (a >= b ? a * a + a + b : a + b * b)
